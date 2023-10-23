240c1f946107b3f178421d3be16472e3538bca9d
angie2512 <a20191792@pucp.edu.pe>
2023-10-23 16:49:38 -0500

    avance vistas

---
+++ app/src/main/java/com/example/lab5_iot/Trabajador/Vistatrabajador.java
@@ -0,0 +1,16 @@
+package com.example.lab5_iot.Trabajador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab5_iot.R;
+
+public class Vistatrabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vistatrabajador);
+    }
+}
---
+++ app/src/main/java/com/example/lab5_iot/Tutor/Listatrabajadores.java
@@ -0,0 +1,16 @@
+package com.example.lab5_iot.Tutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab5_iot.R;
+
+public class Listatrabajadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listatrabajadores);
+    }
+}
