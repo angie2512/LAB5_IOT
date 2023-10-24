68a2ae3dad8ffd377e8cfb9f3bdd704ca1c5b56b
alexia2212 <a20192270@pucp.edu.pe>
2023-10-23 23:27:13 -0500

    main activity binding

--- app/src/main/java/com/example/lab5_iot/MainActivity.java
+++ app/src/main/java/com/example/lab5_iot/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.lab5_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.lab5_iot.Trabajador.Vistatrabajador;
+import com.example.lab5_iot.Tutor.Vistatutor;
+import com.example.lab5_iot.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.buttonTrabajador.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Vistatrabajador.class);
+            startActivity(intent);
+        });
+
+        binding.buttonTutor.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Vistatutor.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
