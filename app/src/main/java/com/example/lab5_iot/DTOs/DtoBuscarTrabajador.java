fc2591bebf9dbf46d3c6d1fcd5e0109be70450bb
alexia2212 <a20192270@pucp.edu.pe>
2023-10-24 00:17:34 -0500

    dto trabajador

--- app/src/main/java/com/example/lab5_iot/DTOs/DtoBuscarTrabajador.java
+++ app/src/main/java/com/example/lab5_iot/DTOs/DtoBuscarTrabajador.java
@@ -1,6 +1,24 @@
 package com.example.lab5_iot.DTOs;
 
+import com.example.lab5_iot.Entidades.Employees;
+
 public class DtoBuscarTrabajador {
+    private Employees empleado;
+    private String respuesta;
 
+    public Employees getEmpleado() {
+        return empleado;
+    }
 
+    public void setEmpleado(Employees empleado) {
+        this.empleado = empleado;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
 }
